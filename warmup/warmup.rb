7a32410be14f2f16317df20f946a5b4366e67b2b
Candace Eckels <candace.eckels@gmail.com>
2023-02-19 15:54:53 -0700

    Warmup complete

--- warmup/warmup.rb
+++ warmup/warmup.rb
@@ -4,10 +4,15 @@ nested_animals = [[:dog, :cat, :zebra], [:quokka, :unicorn, :bear]]
 # Use an enumerable to:
 # 1. Return an unnested (single layer) array of animals as strings
 
-
+  p nested_animals.flatten
 
 # 2. Return an unnested array of animals with length >= 4
 
-
+p nested_animals.flatten.map { |animal| animal if animal.to_s.length >= 4}.compact
 
 # 3. Return a hash where the keys are the animal, and the values are the length. ex: {dog: 3, cat: 3, zebra: 5 ... }
+hash = {}
+nested_animals.flatten.each do |animal|
+  hash[animal] = animal.length
+end
+p hash
